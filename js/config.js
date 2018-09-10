8749c0db05b4166d85a7fccdfe2b628edb77f628
Benjamin Klaus <klaus@technikum-wien.at>
2018-09-10 16:52:20 +0200

    added finnish translations, changed project id

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ window.C = {};
 C.translations = {};
 
 ////// can be changed: ///////
-C.projectId = 'demo-project'; //important: change this ID when copying/forking this project
+C.projectId = 'sarajarvi-house-project-costa-rica'; //important: change this ID when copying/forking this project
 C.lowerLimit = 1; //the lowest amount that is possible to spend
 C.upperLimit = 350; //the highest amount that is possible to spend
 C.doneAmounts = [5,6,10,300]; //amounts that are verified to be already done (donation already received)
@@ -16,14 +16,16 @@ C.redirectDelayMs = 1000; //delay in milliseconds, before the redirect is done (
 
 C.translations['confirmDonate'] = {
     'en' : "Do you want to donate the amount of {0}{1}? Clicking on 'OK' will mark this amount as done and redirect you to a page with payment information.",
-    'de' : 'Möchtest du wirklich den Betrag von {0}{1} spenden? Ein Klick auf "OK" wird den Betrag als "gespendet" und dich zu einer Seite mit Zahlungsinformationen weiterleiten.'
+    'de' : 'Möchtest du wirklich den Betrag von {0}{1} spenden? Ein Klick auf "OK" wird den Betrag als "gespendet" und dich zu einer Seite mit Zahlungsinformationen weiterleiten.',
+    'fi' : 'Haluatko lahjoittaa summan {0}{1}? Painamalla ”OK” summa tulee valituksi ja sinut ohjataan sivulle, jossa on lahjoitustiedot.'
 };
 C.translations['confirmUndo'] = {
     'en' : "Do you really want to unmark the amount {0}{1}? Please only do this if you clicked on this button before and you now decided to not spend the money.",
-    'de' : 'Möchtest du wirklich die Markierung des Betrags von {0}{1} rückgängig machen? Bitte tu das nur, wenn du zuvor auf den Button geklickt hast und nun entschieden hast den Betrag doch nicht zu spenden.'
+    'de' : 'Möchtest du wirklich die Markierung des Betrags von {0}{1} rückgängig machen? Bitte tu das nur, wenn du zuvor auf den Button geklickt hast und nun entschieden hast den Betrag doch nicht zu spenden.',
+    'fi' : 'Oletko varma, että haluat poistaa valinnan summasta {0}{1} Tee näin ainoastaan, jos painoit nappulaa aiemmin ja päätitkin olla lahjoittamatta summaa.'
 };
 C.translations['redirectURL'] = {
-    'en' : 'https://www.google.at/',
-    'de' : 'https://github.com/dashboard'
+    'en' : 'http://www.sarajarvet.com/support-info.html',
+    'fi' : 'http://sarajarvet.weebly.com/kannatustiedot.html'
 };
 /////////////////////////////
